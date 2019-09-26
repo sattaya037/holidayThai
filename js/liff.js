44a1e6342c521c8237485bd5d08835e595aeb08e
sattaya037 <sattaya.ra@ics-th.com>
2019-09-26 14:24:49 +0700

    make it better

--- js/liff.js
+++ js/liff.js
@@ -5,9 +5,10 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
+    document.getElementById("useridfield").innerHTML = data.context.userId;
     document.getElementById('languagefield').textContent = data.language;
     document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
+    // document.getElementById('useridfield').textContent = data.context.userId;
     document.getElementById('utouidfield').textContent = data.context.utouId;
     document.getElementById('roomidfield').textContent = data.context.roomId;
     document.getElementById('groupidfield').textContent = data.context.groupId;
