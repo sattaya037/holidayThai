e0aa223b11331ac39f5d8b4c92bb57314f4735ce
sattaya037 <sattaya.ra@ics-th.com>
2019-09-27 11:36:09 +0700

    make it better

--- spare.js
+++ spare.js
@@ -1,8 +1,19 @@
 
 function PDF() {
-    console.log("PDF");
-    var pdf = new jsPDF();
-    pdf.text(30, 30, 'Hello world!');
-    pdf.save('hello_world.pdf');
+    var doc = new jsPDF();
+    doc.text(20, 20, 'Hello world!');
+    doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+    doc.addPage();
+    doc.text(20, 20, 'Do you like that?');
+    
+    // Output as Data URI
+    var string = doc.output('datauri');
+
+    var iframe = "<iframe width='100%' height='100%' src='" + string + "'></iframe>"
+    
+    var x = window.open();
+    x.document.open();
+    x.document.write(iframe);
+    x.document.close();
 }
 
